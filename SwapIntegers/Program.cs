de75c22054b434f258f1aa0be5fe198928370e80
Tarunikaa B <tarunikatiku23@gmail.com>
2025-06-05 19:40:43 +0530

    Add files via upload

---
+++ SwapIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwapIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first integer:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second integer:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Before swapping: First value = {a}, Second value = {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"After swapping: First value = {a}, Second value = {b}");
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        }
+    }
+}
