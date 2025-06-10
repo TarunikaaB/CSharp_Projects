242f8868848a3617069f4cda6ac5771f1096c828
Tarunikaa B <tarunikatiku23@gmail.com>
2025-06-10 22:40:49 +0530

    Add files via upload

---
+++ MultiplicationTable/MultiplicationTableApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{number} x {i} = {number * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+            }
+        }
+    }
+}
