561ea3fe496d95cd50dcd33344cce2b62ea3cb39
Tarunikaa B <tarunikatiku23@gmail.com>
2025-07-05 23:23:51 +0530

    Add files via upload

---
+++ SentenceModifierApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace SentenceModifierApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence:");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine("Enter the word to replace:");
+            string oldWord = Console.ReadLine();
+
+            Console.WriteLine("Enter the new word:");
+            string newWord = Console.ReadLine();
+
+           if (string.IsNullOrWhiteSpace(sentence) || string.IsNullOrWhiteSpace(oldWord) || string.IsNullOrWhiteSpace(newWord))
+            {
+                Console.WriteLine("Error: Arguments cannot be null, empty, or whitespace");
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder(sentence);
+                sb.Replace(oldWord, newWord);
+                string modifiedSentence = sb.ToString();
+                Console.WriteLine("Modified Sentence: " + modifiedSentence);
+            }
+        }
+    }
+}
